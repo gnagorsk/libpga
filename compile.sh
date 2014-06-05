6c8059c0682b7b10a9fd444b659e82afe60090f3
Grzegorz Nagórski <131585ng@des01.eti.pg.gda.pl>
2014-06-05 22:53:58 +0200

    Compiles

---
+++ compile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+nvcc --compiler-bindir=/opt/gcc-4.4.6/bin --gpu-architecture sm_20 -I./include/ -c src/pga.cu -o pga.o
+mpicc -I./include/ -c test/test.c -o test.o
+mpicc test.o pga.o -o mpicuda -L/usr/local/cuda/lib64 -lm -lcudart -lcurand -lstdc++
---
+++ test/test.c
@@ -0,0 +1,43 @@
+/*
+ * test.cu
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <pga.h>
+#include <limits.h>
+#include <float.h>
+#include <stdio.h>
+#include <mpi.h>
+
+#define GENOME_LENGTH 100
+
+int mpi_nodes_count;
+int mpi_my_rank;
+
+int main(int argc, char **argv) {
+  MPI_Init(&argc, &argv);
+
+  pga_t *p = pga_init();
+
+	population_t *pop = pga_create_population(p, 10000, GENOME_LENGTH, RANDOM_POPULATION);
+
+	pga_run(p, 100, 0.f);
+	
+	pga_get_best(p, pop);
+	
+	pga_deinit(p);
+
+  MPI_Finalize();
+	return 0;
+}
